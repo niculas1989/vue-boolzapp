d2c49de33af21ad41dde74c36bbe84ec4b090af2
niculas1989 <nicolas.maranzano@libero.it>
2022-02-02 11:18:18 +0100

    added comment about today's milestones

--- js/script.js
+++ js/script.js
@@ -33,6 +33,16 @@ Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitan
 Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 */
 
+/*
+MILESTONE DEL GIORNO: //! DAY THREE
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+Milestone 5 - opzionale
+Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato (vedi immagine in allegato)
+Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+*/
+
+
 const root = new Vue({
     el: '#root',
     data: {
